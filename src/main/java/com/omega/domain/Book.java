8b99c2c20627134c1dae8fd558b2c40b38cda7b0
Milind Chawla <milind.chawla86@gmail.com>
2016-06-01 14:24:29 +0530

    Book domain complete

--- src/main/java/com/omega/domain/Book.java
+++ src/main/java/com/omega/domain/Book.java
@@ -1,5 +1,62 @@
 package com.omega.domain;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Size;
+
 public class Book {
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
 	
+	@Column
+    @Size(min=3, max=20)
+	private String name;
+	
+	@Column
+	private String image;
+	
+	public Book(String name, String image) {
+		this.id = -1L;
+		this.name = name;
+		this.image = image;
+	}
+	
+	public Book(long id, String name, String image) {
+		this.id = id;
+		this.name = name;
+		this.image = image;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	@Override
+	public String toString() {
+		return "Book (id=" + id + ", name=" + name + ", image=" + image + ")";
+	}
 }
